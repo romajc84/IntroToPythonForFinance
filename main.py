8838c8594f7f6b872ad66de0e59701f228bbf463
romajc84 <joeyroman84@gmail.com>
2020-07-30 11:11:09 -0400

    Ep.5 Automatic Email Alerts

--- main.py
+++ main.py
@@ -1,45 +1,66 @@
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
 import yfinance as yf
 import datetime as dt
 import pandas as pd
 from pandas_datareader import data as pdr
+import time
+
+EMAIL_ADDRESS = ''
+EMAIL_PASSWORD = ''
+
+msg = EmailMessage()
 
 yf.pdr_override()
-start = dt.datetime(1980, 12, 1)
+start = dt.datetime(2018, 12, 1)
 now = dt.datetime.now()
-stock = ""
 
-stock = input("Enter the stock symbol: ")
-while stock != "quit":
+stock = "QQQ"
+TargetPrice = 180
+
+msg["Subject"] = "Alert on " + stock
+msg["From"] = EMAIL_ADDRESS
+msg["To"] = ''
+
+alerted = False
+
+while 1:
+
     df = pdr.get_data_yahoo(stock, start, now)
+    currentClose = df["Adj Close"][-1]
 
-    df.drop(df[df["Volume"] < 1000].index, inplace = True)
+    condition = currentClose > TargetPrice
 
-    dfmonth = df.groupby(pd.Grouper(freq="M"))["High"].max()
-    
-    glDate = 0
-    lastGLV = 0
-    currentDate = ""
-    currentGLV = 0
+    if (condition and alerted == False):
 
-    for index, value in dfmonth.items():
-        if value > currentGLV:
-            currentGLV = value
-            currentDate = index
-            counter = 0
-        if value < currentGLV:
-            counter = counter + 1
-            if counter == 3 and ((index.month != now.month) or (index.year != now.year)):
-                if currentGLV != lastGLV:
-                    print(currentGLV)
-                glDate = currentDate
-                lastGLV = currentGLV
-                counter = 0
+        alerted = True
 
-    if lastGLV == 0:
-        message = stock + " has not formed a green line yet"
+        message = stock + " Has activated the alert price of " + str(TargetPrice) + \
+            "\nCurrent Price: " + str(currentClose)
+
+        msg.set_content(message)
+
+        files = [
+            r"/Users/joeyr/Development/Projects/Python/Learning/PythonForFinanceYT/Fundamental List.xlsx"]
+
+        for file in files:
+            with open(file, "rb") as f:
+                file_data = f.read()
+                file_name = "FundamentalList.xlsx"
+
+                msg.add_attachment(
+                    file_data, maintype="application", subtype='ocetet-stream', filename=file_name)
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            smtp.send_message(msg)
+
+            print('completed')
+
     else:
-        message = ("Last Green Line: " + str(lastGLV) + " on " + str(glDate))
+        print('No new alerts')
 
-    print(message)
-
-    stock = input("Enter the stock symbol: ")
+    time.sleep(60)
